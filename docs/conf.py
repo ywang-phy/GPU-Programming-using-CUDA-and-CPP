c3342a2dbf18aa12f0d4be7f888a43c8e79eeba4
Yuan Wang <yuan.wang.phy@gmail.com>
2025-10-07 08:31:30 +0100

    add and delete

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,20 @@
 # Use the sphinx-book-theme (similar to d2l.ai)
 html_theme = 'sphinx_book_theme'
 
-# Enable markdown support
+
 extensions = [
     'myst_parser',
-    'sphinx.ext.mathjax',  # For math equations
+    'sphinx.ext.mathjax',
     'sphinx.ext.autodoc',
-    'sphinx_copybutton',   # Copy button for code blocks
+    'sphinx_copybutton',
+]
+
+# Optional: Configure MyST to support more markdown features
+myst_enable_extensions = [
+    "dollarmath",      # For $math$ syntax
+    "amsmath",         # For advanced math
+    "deflist",         # Definition lists
+    "colon_fence",     # ::: fences
 ]
 
 # Configure the theme
